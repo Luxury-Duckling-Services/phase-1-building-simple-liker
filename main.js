faacb1c6f111c963d525c9dbdb62ecef7dd61294
Yifan <datasciencewyfan@gmail.com>
2022-05-25 14:47:18 -0700

    commit tree 1bf54bfe74127668c25c1e0a222bbdab28028132

--- main.js
+++ main.js
@@ -4,8 +4,42 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+document.querySelectorAll('.like-glyph').forEach(like => {
+  
+  like.addEventListener('click' , ()=>{
 
+    if (! like.classList.contains('activated-heart') ) {
+      
+      mimicServerCall()
+        .then(res => {
+          like.classList.add('activated-heart')
+          like.textContent = FULL_HEART
+        })
+        .catch(error =>{
+          show(error)
+          setTimeout( hide, 3000)
+        })
+      
+    } else {
+      like.classList.remove('activated-heart')
+      like.textContent = EMPTY_HEART
+    }
 
+  })
+})
+
+const modal = document.getElementById('modal')
+const modalMessage = modal.querySelector('#modal-message')
+
+function show(error) {
+  modalMessage.textContent = error
+  modal.classList.remove('hidden')
+}
+
+function hide() {
+  modal.classList.add('hidden')
+  modalMessage.textContent = ''
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
